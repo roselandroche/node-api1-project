f8780e740df7a47efbc28ae6a7c0cc0b463353a7
roselandroche <r.landroche@gmail.com>
2019-12-11 21:45:24 -0500

    Changing host and port

--- index.js
+++ index.js
@@ -81,8 +81,8 @@ app.delete("/users/:id", async (req, res) => {
     }
 })
 
-const port = 5000
-const host = "127.0.0.1"
+const port = process.env.PORT || 5000
+const host = process.env.HOST || "0.0.0.0"
 
 app.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}`)
